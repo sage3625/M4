521bf3080de8a7f9cbd923d6a9856a9a5a68b6bb
sage3625 <ncs2716@email.vccs.edu>
2026-05-05 13:03:50 -0400

    adding postfix evaluator file updates

--- postfix_evaluator.py
+++ postfix_evaluator.py
@@ -2,7 +2,35 @@
 from stack import Stack
 
 class PostfixEvaluator:
-    """Evaluates postfix expressions using a stack."""
-
+    
     def evaluate(self, expression):
         stack = Stack()
+        tokens = expression.split()
+
+        for token in tokens:
+            if token.isdigit() or self._is_float(token):
+                stack.push(float(token))
+            else:
+                b = stack.pop()
+                a = stack.pop()
+
+                if token == '+':
+                    stack.push(a + b)
+                elif token == '-':
+                    stack.push(a - b)
+                elif token == '*':
+                    stack.push(a * b)
+                elif token == '/':
+                    stack.push(a / b)
+                else:
+                    raise Exception(f"Invalid operator: {token}")
+
+        result = stack.pop()
+        return int(result) if result.is_integer() else result
+
+    def _is_float(self, s):
+        try:
+            float(s)
+            return True
+        except:
+            return False
