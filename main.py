3e6d0bd645ca2898b1e6b979c5cfe8b0e15357bc
sage3625 <ncs2716@email.vccs.edu>
2026-05-05 16:30:27 -0400

    Input the main functin, but not getting the expected results.

--- main.py
+++ main.py
@@ -38,3 +38,35 @@ infix_tests = [
     "( A + B * C ) / ( D - E )",
     "A + ( B - C ) * D",
     "( A + B * ( C - D ) ) / E"
+]
+
+print("\n----- Infix to Postfix Converter -----")
+conv = InfixToPostfixConverter()
+for expr in infix_tests:
+    print(f"[{expr}] -> [{conv.convert(expr)}]")
+# ------------------------------
+# Linked List Tests
+# ------------------------------
+print("\n----- Linked List Tests -----")
+lst = SinglyLinkedList()
+lst.build_list_forward([10, 20, 30, 40, 50])
+print(lst)
+lst.delete_first()
+print(lst)
+lst.delete_last()
+print(lst)
+lst.delete_value(30)
+print(lst)
+
+# ------------------------------
+# Split Evens and Odds
+# ------------------------------
+print("\n----- Split Evens and Odds -----")
+s = SplitEvensOdds()
+s.build_list_forward([1,2,3,4,5,6,7,8,15,14,13,12,11,10,9])
+print(s)
+
+evens, odds = s.split_even_odd()
+print(evens)
+print(odds)
+print(s)  # should be empty
